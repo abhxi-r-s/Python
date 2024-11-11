fcc68020837f1ebfc48a40d0403054fa6b7a0d30
abhxi-r-s <abhishekunni8281@gmail.com>
2024-11-11 14:51:59 +0530

    completed upto 19

---
+++ cycle2/8.pattern.py
@@ -0,0 +1,11 @@
+
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print("*", end=' ')
+    print("\n")
+
+
+for k in range(4, 0, -1): 
+    for m in range(1, k + 1):
+        print("*", end=' ')
+    print("\n")
---
+++ cycle2/9.string.py
@@ -0,0 +1,7 @@
+string=input("Enter the string :")
+string2=input("Enter the second string :")
+
+length=len(string)
+length2=len(string2)
+
+print(string2[0] + string[1:] + string[0]+string2[1:])
